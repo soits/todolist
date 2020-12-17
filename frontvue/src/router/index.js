c5caba16405e91899bcfc041de15d4d7eb82fa01
gesazumo <gesazumoo@gmail.com>
2020-12-17 17:49:43 +0900

    연결 테스트 진행중

---
+++ frontvue/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import ApiCall from '../views/ApiGetTest.vue'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },{
+
+    path : '/apiCall',
+    name : 'ApiCall',
+    component : ApiCall 
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ frontvue/src/views/ApiGetTest.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="example">
+    <h1>This is Api GET Test</h1>
+
+    <br/>
+    <b>데이터가져오기</b>
+    <button v-on:click="get()">클릭</button>
+    
+
+  </div>
+    
+</template>
+
+
+<script>
+
+const method = {
+  get : function(){
+    console.log('test');
+  }
+};
+
+export default method;
+
+</script>
